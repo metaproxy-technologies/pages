88b4fec65d2882be513f958cd4aff7b1e2daeae2
Tomohiko Araki <arakitomohiko@metaproxy.co>
2021-12-13 15:27:51 +0900

    p

--- _src/AdafruitLEDMatrix/nhk.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-#Copyright (c) 2017 Tomohiko Araki
-#Released under the MIT license
-#http://opensource.org/licenses/mit-license.php
-URL="hls://nhkradioakr1-i.akamaihd.net/hls/live/511633/1-r1/1-r1-01.m3u8"
-livestreamer --yes-run-as-root $URL best -p mplayer &
-
